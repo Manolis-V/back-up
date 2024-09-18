19ec7b7ab6894d9a1706f3f511ecfe4b96323043
Manolis Voutetakis <manolisvoutetakis@gmail.com>
2024-09-18 12:47:59 +0300

    color added

--- test.py
+++ test.py
@@ -13,33 +13,29 @@ backup_folder_path = destination_folder / backup_folder_name
 backup_folder_path.mkdir(parents=True, exist_ok=True)
 
 
-def copy_files(temp, file_path, backup_folder_path):
+def copy_files(file_path, backup_folder_path):
 
     for back_up_file_path in backup_folder_path.iterdir():
 
         if back_up_file_path.name == file_path.name and time.ctime(file_path.stat().st_mtime) < time.ctime(back_up_file_path.stat().st_mtime):
-            print("not modified", file_path, back_up_file_path)
-            temp = 1
-            break
-        
-    if temp == 0:
-        shutil.copy(file_path, backup_folder_path)
-        print("copied", file_path)
+            print("\033[33mnot modified", file_path.name, "\033[0m")
+            return
 
+    shutil.copy(file_path, backup_folder_path)
+    print("\033[32mcopied", file_path.name, "\033[0m")
+# \033[31m red
 
 def handle_folders(temp_source_folder, backup_folder_path):
     
     for file_path in temp_source_folder.iterdir():
 
         if file_path.is_file():
-            print("file inside folder")
-            copy_files(0, file_path, backup_folder_path)
+            copy_files(file_path, backup_folder_path)
 
-        if file_path.is_dir():
-            print("folder inside folder")
+        if file_path.is_dir() and file_path.name != '.git':
             backup_folder_path_folder = backup_folder_path / file_path.name
             backup_folder_path_folder.mkdir(parents=True, exist_ok=True)
-            handle_folders(file_path, backup_folder_path)
+            handle_folders(file_path, backup_folder_path_folder)
 
 
 # Copy files from source to destination
@@ -47,7 +43,7 @@ for file_path in source_folder.iterdir():
     
     # handling files
     if file_path.is_file():
-        copy_files(0, file_path, backup_folder_path)
+        copy_files(file_path, backup_folder_path)
 
     # handling folders
     if file_path.is_dir() and file_path.name != '.git':
